1e9801ae8409da87fa977640613df81d3ba3b7d2
Dias Valeev <42158559+DiaZzzFF@users.noreply.github.com>
2024-06-03 10:32:11 +0300

    Добавляет декоративные элементы (#8)

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,1 +1,16 @@
-/* в этот файл добавляет скрипты*/
+const nav = document.querySelector('.nav');
+const navToggle = document.querySelector('.nav__toggle');
+
+nav.classList.remove('nav--nojs');
+
+navToggle.addEventListener('click', () => {
+  if (nav.classList.contains('nav--closed')) {
+    nav.classList.remove('nav--closed');
+    nav.classList.add('nav--opened');
+    navToggle.classList.add('nav__toggle--opened');
+  } else {
+    nav.classList.add('nav--closed');
+    nav.classList.remove('nav--opened');
+    navToggle.classList.remove('nav__toggle--opened');
+  }
+});
